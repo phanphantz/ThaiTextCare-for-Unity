31c1cf5ca37db0278999277c4aa913bbc1a93555
Phun Peeticharoenthum <phunphun484@gmail.com>
2024-10-07 03:11:39 +0700

    FIX: asset setup process for package version

--- Runtime/ThaiTextNurse/ThaiTextCareSettings.cs
+++ Runtime/ThaiTextNurse/ThaiTextCareSettings.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 #endif
 
+using System;
 using System.IO;
 using UnityEngine;
 
@@ -23,7 +24,8 @@ namespace PhEngine.ThaiTextCare
         [Header("Editor-Only")]
         [SerializeField] bool loadDictionaryOnStart = true;
         public bool IsLoadDictionaryOnEditorStartUp => loadDictionaryOnStart;
-        public const string SettingsPath = "Assets/Plugins/ThaiTextCare/Resources/ThaiTextCareSettings.asset";
+        public const string PluginsFolderPath = "Plugins/ThaiTextCare/Resources/";
+        public static string SettingsPath => "Assets/" + PluginsFolderPath + "ThaiTextCareSettings.asset";
         
         public static ThaiTextCareSettings PrepareInstance()
         {
@@ -37,11 +39,27 @@ namespace PhEngine.ThaiTextCare
                     unsafeInstance = CreateInstance<ThaiTextCareSettings>();
                     var directory = Path.GetDirectoryName(path);
                     if (!Directory.Exists(directory) && directory != null)
-                            Directory.CreateDirectory(directory);
+                        Directory.CreateDirectory(directory);
 
                     AssetDatabase.CreateAsset(unsafeInstance, path);
                     AssetDatabase.SaveAssets();
                     Debug.Log("Created a default ThaiTextNurseSettings at : " + path);
+                    
+                    //Asset is added to the project as source code
+                    if (ThaiTextNurse.TryLoadDictionaryAsset(unsafeInstance, out var defaultTextAsset))
+                    {
+                        Resources.UnloadAsset(defaultTextAsset);
+                    }
+                    //Asset is added to the project as package
+                    else
+                    {
+                        var packageDictionary = (TextAsset)AssetDatabase.LoadAssetAtPath("Packages/com.phengine.thaitextcare/Resources/dictionary.txt", typeof(TextAsset));
+                        if (packageDictionary == null)
+                            throw new NullReferenceException("Cannot find the default package dictionary file.");
+                        
+                        File.WriteAllText(Path.Combine(Application.dataPath, PluginsFolderPath) + "dictionary.txt", packageDictionary.text);
+                        AssetDatabase.Refresh();
+                    }
 #else
                     Debug.LogError("ThaiTextNurseSettings.asset is missing from the Resources folder! the default settings will be used on ThaiTextNurse components");
 #endif
