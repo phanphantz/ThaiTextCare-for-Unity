a4432f5516a7a758e067145ddcd7b1ab9a4bf683
Phun Peeticharoenthum <phunphun484@gmail.com>
2024-10-07 11:01:31 +0700

    EDIT: Improve example script

--- Examples/ThaiTextCareExampleScript.cs
+++ Examples/ThaiTextCareExampleScript.cs
@@ -12,11 +12,17 @@ public class ThaiTextCareExampleScript : MonoBehaviour
     void Start()
     {
         inputField.onValueChanged.AddListener(OnValueChanged);
+        RefreshWordCount();
     }
 
     void OnValueChanged(string input)
     {
         outputText.text = input;
+        RefreshWordCount();
+    }
+
+    void RefreshWordCount()
+    {
         wordCountText.text = nurse.LastWordCount.ToString("N0") + " Words";
     }
 }
